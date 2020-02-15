60876a7011e58f82a5039313ba7010f84eb323e0
Max Parshin <43303110+mxprshn@users.noreply.github.com>
2020-02-16 01:08:23 +0300

    Added tasks

---
+++ Homework1/Factorial.fs
@@ -0,0 +1,9 @@
+﻿module Factorial
+
+let rec factorial x acc i =
+    if x < 0 then
+        None
+    else if i = x + 1 then
+        Some(acc)
+    else
+        factorial x (acc * i) (i + 1)
---
+++ Homework1/PowerList.fs
@@ -0,0 +1,19 @@
+﻿module PowerList
+
+let rec power x n acc =
+    if (n = 0) then
+        1
+    else if (n = 1) then
+        x * acc
+    else if (n % 2 = 0) then
+        power (x * x) (n / 2) acc
+    else
+        power (x * x) (n / 2) (acc * x)
+
+let rec powerList n m i acc =
+    if (m < 0) || (n < 0) then
+        None
+    else if i = m + 1 then
+        Some(acc)
+    else
+        powerList n m (i + 1) ((power 2 (n + m - i) 1) :: acc)
