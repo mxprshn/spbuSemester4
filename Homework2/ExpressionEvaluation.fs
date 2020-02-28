8a30a0b3342caca741208c956e3e92f55d0f37ef
mxprshn <mxprshn@gmail.com>
2020-02-28 13:29:14 +0300

    Added some code for expression evaluation

---
+++ Homework2/ExpressionEvaluation.fs
@@ -0,0 +1,17 @@
+﻿module ExpressionEvaluation
+
+type Expression =
+    | Sum of Expression * Expression
+    | Multiple of Expression * Expression
+    | Difference of Expression * Expression
+    | Quotient of Expression * Expression
+    | Operand of int
+
+let rec evaluate = function
+    | Sum(a, b) -> evaluate a + evaluate b
+    | Multiple(a, b) -> evaluate a * evaluate b
+    | Difference(a, b) -> evaluate a - evaluate b
+    | Quotient(a, b) -> evaluate a / evaluate b
+    | Operand(x) -> x
+
+printf "%d" (Sum(Operand(1), Operand(2)))
