168080f2175954e0aa1b8b14abd9a1d334b3b8fb
mxprshn <mxprshn@gmail.com>
2020-03-07 17:23:15 +0300

    Added expression type definition

---
+++ LambdaInterpreter/LambdaInterpreter/Interpreter.fs
@@ -0,0 +1,20 @@
+﻿module Interpreter
+
+type Expression = 
+    | Variable of char
+    | Application of Expression * Expression
+    | Abstraction of char * Expression
+
+let rec betaReduction expression =
+    let rec substitute before after = function
+        | Variable(v) when v = before -> Variable(after)
+        | Variable(v) as x -> x
+        | Application(l, r) -> Application(substitute before after l, substitute before after r) 
+        | Abstraction(v, e) -> Abstraction(v, betaReduction e)
+
+
+    match expression with
+        | Variable(v) as x -> x
+        | Application(l, r) when l :? Abstraction -> 
+        | Abstraction(v, e) -> Abstraction(v, betaReduction e)
+
