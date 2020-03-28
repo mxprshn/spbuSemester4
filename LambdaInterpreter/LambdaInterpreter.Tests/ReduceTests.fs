34ff1452a45196578a1651039d68a49ab6454485
mxprshn <mxprshn@gmail.com>
2020-03-28 14:53:03 +0300

    Added some tests

--- LambdaInterpreter/LambdaInterpreter.Tests/ReduceTests.fs
+++ LambdaInterpreter/LambdaInterpreter.Tests/ReduceTests.fs
@@ -4,6 +4,13 @@ open NUnit.Framework
 open FsUnit
 open Interpreter
 
+let True = Abs('x', Abs('y', Var 'x'))
+let False = Abs('x', Abs('y', Var 'y'))
+let If = Abs('b', Abs('t', Abs('f', App(App(Var 'b', Var 't'), Var 'f'))))
+let And = Abs('a', Abs('b', App(App(App(If, Var 'a'), Var 'b'), False)))
+let Or = Abs('a', Abs('b', App(App(App(If, Var 'a'), True), Var 'b')))
+let Not = Abs('b', App(App(App(If, Var 'b'), False), True))
+
 let testCases =
     [
         App(Abs('x', Var('x')), Var('y')), Var('y')
@@ -14,10 +21,22 @@ let testCases =
         App(Abs('x', Abs('y', Var('x'))), Var('y')), Abs('a', Var('y'))
         App(App(Abs('x', Var('a')), Var('x')), App(Abs('y', Var('b')), Var('y'))), App(Var('a'), Var('b'))
         App(App(Abs('x', Abs('y', Abs('z', App(App(Var('x'), Var('z')), App(Var('y'), Var('z')))))), Abs('x', Abs('y', Var('x')))), Abs('x', Abs('y', Var('x')))), Abs('z', Var('z'))
-
+        App(App(Var('x'), Var('y')), App(Var 'a', Var 'b')), App(App(Var('x'), Var('y')), App(Var 'a', Var 'b'))
+        App(App(And, True), False), False
+        App(App(And, True), True), True
+        App(App(And, False), False), False
+        App(App(And, False), True), False
+        App(App(Or, True), False), True
+        App(App(Or, True), True), True
+        App(App(Or, False), False), False
+        App(App(Or, False), True), True 
+        App(Not, True), False
+        App(Not, False), True
     ] |> List.map (fun (l, e) -> TestCaseData(l, e))
 
 [<TestCaseSource("testCases")>]
 let reduceTest l e =
     l |> reduce |> should equal e
+
+
     
--- LambdaInterpreter/LambdaInterpreter/Interpreter.fs
+++ LambdaInterpreter/LambdaInterpreter/Interpreter.fs
@@ -5,6 +5,19 @@ type Expression =
     | App of Expression * Expression
     | Abs of char * Expression
 
+let rec printExp = function
+    | Var(x) -> printf "%O" x
+    | App(l, r) ->
+        printf "("
+        printExp l
+        printf ") "
+        printf "("
+        printExp r
+        printf ")"
+    | Abs(x, e) ->
+        printf "λ%O." x
+        printExp e 
+
 let rec isFree e v =
     match e with
     | Var(x) -> x = v
@@ -17,21 +30,46 @@ let rec isBound e v =
     | Var(x) -> false
     | App(l, r) -> isBound l v || isBound r v
 
-let rec subs b a = function
-    | Abs(v, e) when not (isFree a v) -> Abs(v, subs b a e)
-    | Var(v) when v = b -> a
-    | Var(v) as x -> x
-    | App(l, r) -> App(subs b a l, subs b a r)
+let rec subs b a exp =
+    match exp with
     | Abs(v, e) as x when v = b -> x
+    | Abs(v, e) when not (isFree a v) -> Abs(v, subs b a e)
     | Abs(v, e) ->
         let newSym = ['a'..'z'] |> List.filter (not << (isFree a)) |> List.head
         Abs(newSym, (subs b a (subs v (Var(newSym)) e)))
-
-let rec reduce = function
+    | Var(v) when v = b -> a
     | Var(v) as x -> x
-    | App(Abs(v, e), r) -> reduce (subs v r e) 
-    | Abs(v, e) -> Abs(v, reduce e)
-    | App(Var(a), Var(b)) as x -> x 
-    | App(l, r) -> reduce (App(reduce l, reduce r))
+    | App(l, r) -> App(subs b a l, subs b a r)
 
-reduce (App(App(Var('x'), Var('y')), App(Var 'a', Var 'b')))
+let rec reduce exp = 
+
+    let rec headReduce u =
+        printf ":: "
+        printExp u
+        printf "\n"
+        match u with
+        | Var(v) as x -> x
+        | Abs(a, b) as x -> x
+        | App(l, r) ->
+            match headReduce l with
+                | Abs(v, e) -> subs v r e
+                | x -> App(x, r)
+
+    printf "| "
+    printExp exp
+    printf "\n"
+    match exp with
+    | Var(v) as x -> x
+    | App(l, r) ->
+        match headReduce l with
+            | Abs(v, e) -> reduce (subs v r e)
+            | x -> App(reduce x, reduce r)
+    | Abs(v, e) -> Abs(v, reduce e)
+
+let True = Abs('x', Abs('y', Var 'x'))
+let False = Abs('x', Abs('y', Var 'y'))
+let If = Abs('b', Abs('t', Abs('f', App(App(Var 'b', Var 't'), Var 'f'))))
+let And = Abs('a', Abs('b', App(App(App(If, Var 'a'), Var 'b'), False)))
+let Or = Abs('a', Abs('b', App(App(App(If, Var 'a'), True), Var 'b')))
+
+reduce (App(App(Or, True), False))
