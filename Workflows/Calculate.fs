8e34dc08f4362401ce41a5e1eb97e10d762b9dd7
mxprshn <mxprshn@gmail.com>
2020-03-29 23:02:14 +0300

    Added spaces

--- Workflows/Calculate.fs
+++ Workflows/Calculate.fs
@@ -2,9 +2,9 @@
 
 /// Workflow builder for operations with string integer numbers.
 type CalculateBuilder() =
-    member this.Bind (x: string, f) =
+    member this.Bind (x : string, f) =
         match System.Int32.TryParse x with
         | (true, number) -> number |> f
         | _ -> "NaN"
-    member this.Return (x: int) =
+    member this.Return (x : int) =
         x.ToString()
