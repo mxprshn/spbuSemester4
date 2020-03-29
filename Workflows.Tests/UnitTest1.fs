997df7c6b7262d0f2a70732ece847abbade986fe
mxprshn <mxprshn@gmail.com>
2020-03-29 10:19:35 +0300

    Added project files

---
+++ Workflows.Tests/UnitTest1.fs
@@ -0,0 +1,9 @@
+module Workflows.Tests
+
+open NUnit.Framework
+open FsUnit
+open FsCheck
+
+[<Test>]
+let Test1 () =
+    Assert.Pass()
---
+++ Workflows/Rounding.fs
@@ -0,0 +1,20 @@
+﻿module Rounding
+
+type RoundingBuilder(prec:int) =
+    member this.Bind(x:float, f) =
+        let rounded = System.Math.Round(x, prec)
+        printfn "%f" rounded
+        rounded |> f
+    member this.Return(x:float) =
+        System.Math.Round(x, prec)
+
+let rounding = RoundingBuilder
+
+let oloo =  rounding 3 {
+    let! a = 2.0 / 12.0
+    let! b = 3.5
+    return a / b
+}
+
+
+
