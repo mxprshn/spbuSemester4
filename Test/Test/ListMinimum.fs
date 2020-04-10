ac9138e874d3fc1395113f82dc8c615640c1cf9d
Max Parshin <43303110+mxprshn@users.noreply.github.com>
2020-04-10 18:56:03 +0300

    Added everything

---
+++ Test/Test/ListMinimum.fs
@@ -0,0 +1,8 @@
+﻿module Minimum
+
+open System
+
+/// Finds minimum in the list.
+let minimum = function
+| [] -> None
+| l -> Some(l |> List.fold (fun acc i -> if i < acc then i else acc) Int32.MaxValue)
---
+++ Test/Test/Square.fs
@@ -0,0 +1,21 @@
+﻿module Square
+
+/// Prints square of with the specified size length.
+let printSquare n =
+    let rec printSquareRecursive n x y =
+        match (x, y) with
+        | _ when y = n -> printf ""
+        | _ when x = n ->
+            printf "\n"
+            printSquareRecursive n 0 (y + 1)
+        | _ when y = 0 || y = n - 1 || x = 0 || x = n - 1 ->
+            printf "*"
+            printSquareRecursive n (x + 1) y
+        | _ -> 
+            printf " "
+            printSquareRecursive n (x + 1) y
+    if n <= 0 then
+        printf("Square size should be positive.")
+    else
+        printSquareRecursive n 0 0
+
