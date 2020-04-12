cbf1b45c2504f3fc56c1be6888253b6f674dc175
Max Parshin <43303110+mxprshn@users.noreply.github.com>
2020-04-12 14:53:33 +0300

    Added phonebook class

---
+++ Phonebook/Phonebook/Phonebook.fs
@@ -0,0 +1,18 @@
+﻿module Phonebook
+
+type Phonebook(records) =
+    let records = Map<string, string> records
+    member this.Records with private get () = records
+
+    new () = Phonebook []
+
+    static member FindNumber (book: Phonebook) name = Map.tryFind name book.Records
+    static member FindName (book: Phonebook) number = Map.tryFindKey (fun na nu -> nu = number) book.Records
+    static member Add (book: Phonebook) name number = book.Records |> Map.add name number |> Map.toList |> Phonebook
+    static member ToList (book: Phonebook) = book.Records |> Map.toList
+    static member FromFile book path = System.IO.File.ReadLines path |> Seq.chunkBySize 2 |>
+        Seq.fold (fun b l -> if l.Length = 2 then Phonebook.Add b l.[0] l.[1] else b) book
+    static member ToFile (book: Phonebook) path = System.IO.File.WriteAllLines (path, book.Records |> Map.toList |>
+        List.map (fun (na, nu) -> [na; nu]) |> List.concat)
+    static member NameExists (book: Phonebook) name = Map.containsKey name book.Records
+    static member NumberExists (book: Phonebook) number = Map.exists (fun na nu -> nu = number) book.Records
---
+++ Phonebook/Phonebook/Program.fs
@@ -0,0 +1,56 @@
+﻿open System
+open Phonebook
+
+[<EntryPoint>]
+let main argv =
+    let rec phoneBookLoop book =
+        match Console.ReadLine () with
+        | "0" -> 0
+        | "1" ->
+            printfn "%A" (Phonebook.ToList book)
+            phoneBookLoop book
+        | "2" ->
+            printf "Enter name: "
+            let name = Console.ReadLine ()
+            printf "Enter number: "
+            let number = Console.ReadLine ()
+
+            if Phonebook.NameExists book name then
+                printfn "The name already exists in the book."
+                phoneBookLoop book
+            elif Phonebook.NumberExists book number then
+                printfn "The number already exists in the book."
+                phoneBookLoop book
+            else phoneBookLoop <| Phonebook.Add book name number
+        | "3" -> 
+            printf "Enter name to find the number: "
+            let name = Console.ReadLine ()
+            match Phonebook.FindNumber book name with
+            | None ->
+                printfn "The name does not exist."
+                phoneBookLoop book
+            | Some(number) ->
+                printfn "%s : %s" name number
+                phoneBookLoop book
+        | "4" ->
+            printf "Enter number to find the name: "
+            let number = Console.ReadLine ()
+            match Phonebook.FindName book number with
+            | None ->
+                printfn "The number does not exist."
+                phoneBookLoop book
+            | Some(name) ->
+                printfn "%s : %s" name number
+                phoneBookLoop book
+        | "5" ->
+            printf "Enter path of the file to read data from: "
+            let path = Console.ReadLine ()
+            phoneBookLoop <| Phonebook.FromFile book path
+        | "6" ->
+            printf "Enter path of the file to write data: "
+            let path = Console.ReadLine ()
+            Phonebook.ToFile book path
+            phoneBookLoop book
+        | _ -> phoneBookLoop book
+
+    phoneBookLoop <| Phonebook ()
