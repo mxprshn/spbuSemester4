21cb5c86ddcc16175a4f6301dbf48d847b77e4a1
Max Parshin <43303110+mxprshn@users.noreply.github.com>
2020-05-04 15:31:23 +0300

    Replaced anglesharp with fsharp data and added tests

--- HtmlCount/HtmlCount.Tests/UnitTest1.fs
+++
@@ -1,9 +0,0 @@
-module HtmlCount.Tests
-
-open NUnit.Framework
-
-[<Test>]
-let Test1 () =
-    let ololo = HtmlProcessor (HtmlLoader ())
-    Async.RunSynchronously (ololo.CountLinkPagesSymbols "http://hwproj.me/courses/34")
-    Assert.Pass()
--- HtmlCount/HtmlCount/HtmlCount.fs
+++ HtmlCount/HtmlCount/HtmlCount.fs
@@ -1,39 +1,62 @@
 ﻿module HtmlCount
 
-open AngleSharp
+open FSharp.Data
 open System.Net
 open System.IO
+open System.Text.RegularExpressions
 
+/// Interface for loading html webpages.
 type IHtmlLoader =
+
+    /// Loads a web page as html string.
     abstract member LoadHtmlAsync : url: string -> string Async
 
+/// Class for loading html webpages.
 type HtmlLoader() =
+
     interface IHtmlLoader with
+    /// Loads a web page as html string.
         member this.LoadHtmlAsync url =
             async {
                 let request = WebRequest.Create url
-                use! response = request.AsyncGetResponse ()
-                use stream = response.GetResponseStream ()
-                use reader = new StreamReader (stream)
-                return! Async.AwaitTask <| reader.ReadToEndAsync ()
+                use! response = request.AsyncGetResponse()
+                use stream = response.GetResponseStream()
+                use reader = new StreamReader(stream)
+                return! Async.AwaitTask <| reader.ReadToEndAsync()
             }
 
+/// Class for performing operations with html webpages.
 type HtmlProcessor(loader: IHtmlLoader) =
-    let context = BrowsingContext.New Configuration.Default
 
-    let findLinks (htmlText: string) =
+    let urlRegex = "^(https:|http:)\/\/\S*"
+
+    let findLinks(htmlText: string) =
         async {
-            let! document = htmlText |> context.OpenAsync |> Async.AwaitTask;
-            return document.QuerySelectorAll "a" |> Seq.map (fun e -> e.GetAttribute "href")
+            let document = HtmlDocument.Parse htmlText
+            let links = 
+                document.Descendants ["a"] |>
+                Seq.choose (fun n -> n.TryGetAttribute "href") |>
+                Seq.map (fun a -> a.Value ()) |>
+                Seq.filter (fun s -> Regex.IsMatch(s, urlRegex))
+            return links
+        }
+        
+    let countSymbols(url: string) =
+        async {
+            let! loading = url |> loader.LoadHtmlAsync |> Async.StartChild
+            let! html = loading
+            return url, html.Length
         }
 
-    member this.CountLinkPagesSymbols (url: string) =
-        async {
+    /// Loads all pages linked from the specified one and counts the number of symbols on them.
+    member this.CountLinkedPagesSymbols(url: string) =
+        async {            
             let! loading = url |> loader.LoadHtmlAsync |> Async.StartChild
             let! baseHtml = loading
             let! links = baseHtml |> findLinks
-            do links |> Seq.iter (fun s -> printfn "%s" s)
+            let! results = links |> Seq.map countSymbols |> Async.Parallel
+            results |> Seq.iter (fun r ->
+                match r with
+                | (u, c) -> printfn "%s --- %i" u c)   
+            return results
         }
-
-let ololo = HtmlProcessor (HtmlLoader ())
-Async.RunSynchronously (ololo.CountLinkPagesSymbols "http://hwproj.me/courses/34")
